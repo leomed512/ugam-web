0cb45848f2692aeccbf1fd73d6dec2028913211b
leomed512 <leomedinaforest@gmail.com>
2022-08-07 21:47:19 -0400

    animation for contact form ready

--- scripts/emailAlert.js
+++ scripts/emailAlert.js
@@ -17,3 +17,32 @@ if (alertTrigger) {
     alert('Nuestro equipo se pondrá en contacto contigo muy pronto para atender tu mensaje!', 'success')
   })
 }
+
+const elementAnimate = document.querySelector('#contactAnimation');
+
+
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
+
+  const animateThis = document.querySelector('#animateButton')
+if (animateThis) {
+  animateThis.addEventListener('click', () => { animateCSS('#contactAnimation', 'rubberBand')})
+}
+  
+
