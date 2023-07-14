6ce88daa14b0ceb1c327f32cd01ff94452887991
leomed512 <leomedinaforest@gmail.com>
2023-07-14 12:10:37 -0400

    novedades section added

---
+++ scripts/displaySections.js
@@ -0,0 +1,37 @@
+function goToSection(clase, tab) {
+    localStorage.clear()
+    localStorage.setItem('claseActiva', clase);
+    localStorage.setItem('tabActivo', tab);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var claseActiva = localStorage.getItem('claseActiva');
+    var tabActivo = localStorage.getItem('tabActivo');
+    console.log(claseActiva, tabActivo);
+    if (claseActiva) {
+
+        var seccion = document.getElementById(claseActiva);
+        var tabulador = document.getElementById(tabActivo);
+        //
+        console.log(seccion)
+        console.log(tabulador)
+        const sections = document.querySelectorAll('.pestana');
+        sections.forEach((section) => {
+            section.classList.remove('active');
+            section.classList.remove('show');
+
+        });
+        //
+        const tabs = document.querySelectorAll('.list-group-item');
+        tabs.forEach((section) => {
+            section.classList.remove('active');
+        });
+
+        if (seccion) {
+            seccion.classList.add('active');
+            seccion.classList.add('show');
+            tabulador.classList.add('active');
+
+        }
+    }
+});
