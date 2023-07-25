9d3a7a95f269d1f59a48c6b8ab565eddf872f3b6
leomed512 <leomedinaforest@gmail.com>
2023-07-25 11:26:54 -0400

    script for quoting

---
+++ scripts/cotizar.js
@@ -0,0 +1,17 @@
+function displaContact() {
+    let cont = document.getElementById('infoCont');
+    if (cont.style.display === 'none') {
+        cont.style.display = 'block';
+    } else {
+        cont.style.display = 'none';
+    }
+}
+
+function displaEmailForm() {
+    let cont = document.getElementById('cotizarFormContainer');
+    if (cont.style.display === 'none') {
+        cont.style.display = 'block';
+    } else {
+        cont.style.display = 'none';
+    }
+}
