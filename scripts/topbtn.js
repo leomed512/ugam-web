0283129c12f9ed3e0699b791213c47ce68e7498a
leomed512 <leomedinaforest@gmail.com>
2023-10-08 12:09:34 +0200

    top button done

--- scripts/topbtn.js
+++ scripts/topbtn.js
@@ -1,41 +1,20 @@
-// Espera a que el documento esté completamente cargado antes de ejecutar el código
-document.addEventListener("DOMContentLoaded", function () {
-    const scrollTopButton = document.getElementById("scrollTopButton");
+// script.js
 
-    // Muestra u oculta el botón según la posición de desplazamiento de la página
-    window.onscroll = function () {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollTopButton = document.querySelector(".scroll-top-btn");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
             scrollTopButton.style.display = "block";
         } else {
             scrollTopButton.style.display = "none";
         }
+    });
 
-        // Calcula la altura de la ventana y el contenido de la página
-        const windowHeight = window.innerHeight;
-        const documentHeight = Math.max(
-            document.body.scrollHeight,
-            document.documentElement.scrollHeight
-        );
-
-        // Calcula la posición fija del botón al final de la página
-        if (
-            documentHeight - (window.scrollY + windowHeight) < 100 &&
-            scrollTopButton.style.position !== "absolute"
-        ) {
-            scrollTopButton.style.position = "absolute";
-            scrollTopButton.style.bottom = "20px";
-        } else if (
-            documentHeight - (window.scrollY + windowHeight) >= 100 &&
-            scrollTopButton.style.position !== "fixed"
-        ) {
-            scrollTopButton.style.position = "fixed";
-            scrollTopButton.style.bottom = "20px";
-        }
-    };
-
-    // Vuelve al principio de la página cuando se hace clic en el botón
     scrollTopButton.addEventListener("click", function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     });
 });
