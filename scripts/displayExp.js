b26c2389ef05c0830ad32ec1bb6a1fe11900f81a
leomed512 <leomedinaforest@gmail.com>
2023-10-09 11:43:43 +0200

    gotosection function works, but won't display list

--- scripts/displayExp.js
+++ scripts/displayExp.js
@@ -1,8 +1,9 @@
-function goToSectionNew(accordionItemId, listItemId, tabItemId) {
+function goToSectionNew(accordionItemId, listItemId, tabItemId, containerId) {
     // Almacena la pestaña activa en localStorage
     localStorage.setItem('activeAccordionItem', accordionItemId);
     localStorage.setItem('activeListItem', listItemId);
     localStorage.setItem('activeTabItem', tabItemId);
+    localStorage.setItem('activeParentTabItem', containerId);
 
     // Redirige a expeditions.html
     window.location.href = './expeditions.html';
