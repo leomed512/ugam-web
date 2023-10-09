edda66a14f35c32164e375c19142d36717038ada
leomed512 <leomedinaforest@gmail.com>
2023-10-09 12:48:36 +0200

    gotosection buttons from homepage are working

--- scripts/displayExp.js
+++ scripts/displayExp.js
@@ -1,5 +1,4 @@
 function goToSectionNew(accordionItemId, listItemId, containerId) {
-    // Almacena la pestaña activa en localStorage
     localStorage.setItem('activeAccordionItem', accordionItemId);
     localStorage.setItem('activeListItem', listItemId);
     localStorage.setItem('activeParentTabItem', containerId);
@@ -7,3 +6,4 @@ function goToSectionNew(accordionItemId, listItemId, containerId) {
     // Redirige a expeditions.html
     window.location.href = './expeditions.html';
 }
+//toma 3 parámetros: 1. el link que redirige al contenedor con toda la información del destino turistico que se encuentra en <li><a href=#idContenedor> Ese debe ser el mismo id del contenedor destino. 2. el id de la pestaña que corresponde al destino en <li><a id="pestaña". 3. el id del contenedor padre, bien sea "grandes cumbres", "cumbres para todos etc"
